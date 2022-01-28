0200b96313a9e134a53d076fa3f4c6aef5446f7b
German <germanmarpar21@gmail.com>
2022-01-27 21:47:27 -0300

    weather card ready

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 	import WeatherCard from "./WeatherCard.svelte";
 	let coords = [];
 	
-	const fetchOpenWeather = async (lat, lon) => {
+	const fetchOpenWeather = async (lon, lat) => {
 		const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${OPEN_WEATHER_API_KEY}`);
 		const data = response.json();
 		return data;
@@ -13,7 +13,7 @@
 <Geolocation getPosition bind:coords />
 
 <header>
-	Svelte weather-app
+	Svelte Weather-App
 </header>
 
 {#await fetchOpenWeather(coords[0], coords[1])}
--- src/WeatherCard.svelte
+++ src/WeatherCard.svelte
@@ -1,3 +1,34 @@
 <script>
     export let data;
 </script>
+
+<div class="card">
+    <h1>Weather in {data.name}</h1>
+    <h2>
+        {data.weather[0].main}
+    </h2>
+    <img src="icons/{data.weather[0].main}.png" alt="{data.weather[0].main}">
+    <ul>
+        <li>Temperature: {data.main.temp}°</li>
+        <li>Humidity: {data.main.humidity}%</li>
+        <li>Wind Speed: {data.wind.speed}m/s</li>
+    </ul>
+</div>
+
+<style>
+
+    .card {
+        background-color: #f0f0f0;
+        text-align: center;
+        margin-left: 30%;
+        margin-right: 30%;
+        margin-bottom: 20px;
+        border: 1px solid black;
+        border-radius: 20px;
+    }
+
+    ul {
+        text-align: left;
+        list-style-type: none;
+    }
+</style>
